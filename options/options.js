0b59d98041a1a6aa62c319b45df52c85b73fe8d6
Crowdbotics <team@crowdbotics.com>
2023-08-11 16:42:25 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "waterappp-42792",
+  url: "https://waterappp-42792.botics.co",
+  api: "https://waterappp-42792.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
